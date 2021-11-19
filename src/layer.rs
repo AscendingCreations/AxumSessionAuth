597bbd70f181b592e343fda63900027bc969c1bb
Andrew Wheeler <genusistimelord@gmail.com>
2021-11-19 14:55:09 -0500

    Revert "changed to full blown service"
    This reverts commit c9811f047bf97f68abb169e5344b148c22d53753.

--- src/layer.rs
+++ src/layer.rs
@@ -1,38 +1,19 @@
-use crate::{AuthSessionManager, Authentication};
+use axum::AddExtensionLayer;
 use sqlx::postgres::PgPool;
-use std::marker::PhantomData;
-use tower_layer::Layer;
 
 /// Used to create and store the Extensions Data.
 #[derive(Clone, Debug)]
-pub struct AuthSessionLayer<D> {
+pub struct AuthSessionLayer {
     pub(crate) poll: Option<PgPool>,
     pub(crate) anonymous_user_id: Option<i64>,
-    phantom: PhantomData<D>,
 }
 
-impl<D> AuthSessionLayer<D>
-where
-    D: 'static + Sync + Send + Authentication<D>,
-{
+impl AuthSessionLayer {
     /// Creates a Extension so it can be accessed Directly within Requests.
-    pub fn new(poll: Option<PgPool>, anonymous_user_id: Option<i64>) -> Self {
-        Self {
+    pub fn new(poll: Option<PgPool>, anonymous_user_id: Option<i64>) -> AddExtensionLayer<Self> {
+        AddExtensionLayer::new(Self {
             poll,
             anonymous_user_id,
-            phantom: PhantomData,
-        }
-    }
-}
-
-impl<S, D> Layer<S> for AuthSessionLayer<D>
-where
-    D: 'static + Sync + Send + Authentication<D>,
-{
-    type Service = AuthSessionManager<S, D>;
-
-    ///This is called as soon as the session layer is placed within .layer of axum.
-    fn layer(&self, service: S) -> Self::Service {
-        AuthSessionManager::new(service, self.poll.clone(), self.anonymous_user_id.clone())
+        })
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
 #![doc = include_str!("../README.md")]
 //Todo: Support more databases and expand the Tokio/RLS or RustRLS Selections for SQLx
 mod auth;
-mod future;
 ///This Library Requires that Tower_Cookies and AxumSQLxSessions is used as an active layer.
 mod layer;
-mod manager;
 mod session;
 
 pub use auth::{Auth, HasPermission, Rights};
-pub use future::ResponseFuture;
 pub use layer::AuthSessionLayer;
-pub use manager::AuthSessionManager;
 pub use session::{AuthSession, Authentication};
